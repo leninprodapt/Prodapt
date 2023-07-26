c286094e3346759a4ce81ab00033ccbf6a3909fd
GayathriR80376 <140167544+GayathriR80376@users.noreply.github.com>
2023-07-26 17:01:47 +0530

    Add files via upload

---
+++ Gayathri. R/class work/pojoclass/employee.java
@@ -0,0 +1,32 @@
+class employee
+{
+int emp_id;
+int emp_sal;
+String emp_name;
+	
+public void setid(int id)
+{
+  emp_id=id;
+}
+public int getid()
+{
+return emp_id;
+}
+public void setsal(int sal)
+{
+emp_sal=sal;
+}
+public int getsal()
+{
+return emp_sal;
+}
+public void setname(String name)
+{
+emp_name=name;
+}
+public String getname()
+{
+return emp_name;
+}
+}
+
