d4b771ab4e3b8754a50fcb1e0996560be37e3b87
SNEHA L <84729040+Sneha-15-2001@users.noreply.github.com>
2023-07-26 14:54:08 +0530

    StudentDetails.java

--- Sneha L/Class Work/StudentDetails.java
+++ Sneha L/Class Work/StudentDetails.java
@@ -25,8 +25,8 @@ class StudentDetails
 	public static void main(String args[])
 	{
 		Scanner sc=new Scanner(System.in);
-		StudentDetails stuArr[]=new StudentDetails[4];
-		for(int i=0;i<3;i++)
+		StudentDetails stuArr[]=new StudentDetails[5];
+		for(int i=0;i<4;i++)
 		{
 			System.out.println("Enter Student ID:");
 			int id=sc.nextInt();
