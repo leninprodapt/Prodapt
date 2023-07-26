b48d03e6667104fa6b7a3617b025652f372ae597
Ragz01 <72142578+Ragz01@users.noreply.github.com>
2023-07-26 16:25:25 +0530

    Add files via upload

---
+++ Manicka Raghavi.B/CLASSWORK/Attendance.java
@@ -0,0 +1,30 @@
+import java.lang.String;
+class Attendance 
+{ 
+
+void markAttendance() 
+{ 
+System.out.println("NO STUDENTS PRESENT");
+}
+
+void markAttendance(String...s) 
+{ 
+ 
+System.out.println("No.of Students Present:"+ s.length);
+System.out.println("PRESENT Students are:");
+
+for(String name:s) 
+{
+System.out.println(name);
+}
+
+}
+
+public static void main(String[]args) 
+{ 
+Attendance object=new Attendance();
+object.markAttendance(); 
+object.markAttendance("raghavi","hari","varsha");
+}
+
+} 
